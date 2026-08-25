a712a569807c54a952a272dc22dbac0977e9f9d7
t <t@t>
2026-08-25 23:05:52 +0100

    test: older pin to exercise the bump path

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     hyprland.url = "github:hyprwm/Hyprland/0bd11c7a04a63d2785abd53363f09d552175d67d";
 
     omarchy = {
-      url = "github:basecamp/omarchy/v4.0.1";
+      url = "github:basecamp/omarchy/v4.0.0";
       flake = false;
     };
 
